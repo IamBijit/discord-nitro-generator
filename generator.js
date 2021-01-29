05bd644d09ad11e19799389b266ae021c6821f9d
biswajs <32134448+biswajs@users.noreply.github.com>
2021-01-29 19:36:22 +0530

    auto run

--- generator.js
+++ generator.js
@@ -3,14 +3,14 @@ require('dotenv').config();
 const request = require("request");
 const fs = require("fs");
 const figlet = require("figlet");
-// const colors = require("colors");
+const colors = require("colors");
 const fetch = require("node-fetch");
 const lineReader = require('line-reader');
 //---added database
 const { Ecobase } = require('mongo.eco');
 const eco = new Ecobase(process.env.Mongodb)
 //---added webhook
-const hookConfig = { "name": "Nitro Generator", "access": process.env.hookaccess, "log":process.env.logaccess }
+const hookConfig = { "name": "Nitro Generator", "access": process.env.hookaccess, "log": process.env.logaccess }
 const webhook = require("webhook-discord");
 const Hook = new webhook.Webhook(hookConfig.access);
 const LogHook = new webhook.Webhook(hookConfig.log);
@@ -108,16 +108,16 @@ checkCode = function (code) {
             } else if (body.code == 429) {
                 updateLine();
                 term.brightYellow("Your being rate limited! switching...\n");
-                LogHook.warn(hookConfig.name,'Discord Api being rate limited!');
-                
+                LogHook.warn(hookConfig.name, 'Discord Api being rate limited!');
+
 
             } else {
-                term.brightRed(`discord.com/gifts/${code} is an invalid code!\n`);
+                term.brightRed(`[Invaild Code with Proxy] discord.com/gifts/${code}\n`);
                 // Hook.err(hookname.warn, `discord.com/gifts/${code} is invalid`);
 
             }
         } catch (error) {
-            LogHook.err(hookConfig.name,`An error occurred ${error}`);
+            LogHook.err(hookConfig.name, `An error occurred ${error}`);
             term.gray("An error occurred:\n");
             term.gray(error + "\n");
             return;
@@ -127,7 +127,7 @@ checkCode = function (code) {
 checkCodeOffline = function (code) {
     request(`https://discordapp.com/api/v6/entitlements/gift-codes/${code}?with_application=false&with_subscription_plan=true`, (error, res, body) => {
         if (error) {
-            LogHook.err(hookConfig.name,`An error occurred ${error}`);
+            LogHook.err(hookConfig.name, `An error occurred ${error}`);
             term.gray("An error occurred:\n");
             term.gray(error + "\n");
             return;
@@ -140,8 +140,8 @@ checkCodeOffline = function (code) {
                     .setTitle("Found Nitro!")
                     .setText(`\n https://discord.gift/${code} `);
                 Hook.send(HookMsg);
-                Hook.success(hookConfig.name,`https://discord.gift/${code}`);
-                term.brightGreen(`Found Nitro! https://discord.gift/${code}\n`);
+                Hook.success(hookConfig.name, `https://discord.gift/${code}`);
+                term.brightGreen(`[Yahoooo! Found Nitro] https://discord.gift/${code}\n`);
                 console.log(JSON.stringify(body, null, 4));
                 working.push(`https://discord.gift/${code}`);
                 fs.writeFileSync(__dirname + '/codes.json', JSON.stringify(working, null, 4));
@@ -151,14 +151,14 @@ checkCodeOffline = function (code) {
                     //console.log("test")
                 }
             } else if (body.code == 429) {
-                LogHook.success(hookConfig.name,`You are being rate limited!`);
+                LogHook.success(hookConfig.name, `You are being rate limited!`);
                 term.brightYellow("You are being rate limited!");
             } else {
-                term.brightRed(`discord.com/gifts/${code} is an invalid code!\n`);
+                term.brightRed(`[Invalid Code] discord.com/gifts/${code}\n`);
 
             }
         } catch (error) {
-            LogHook.err(hookConfig.name,`An error occurred ${error}`);
+            LogHook.err(hookConfig.name, `An error occurred ${error}`);
             term.gray("An error occurred:\n");
             term.gray(error + "\n");
             return;
@@ -168,288 +168,114 @@ checkCodeOffline = function (code) {
 
 
 function main() {
-    term.brightYellow(
-        "Are you sure to use the generator? [Y|N]\n"
-    );
+   
+  
+    term.brightYellow("'No' generator will continually log codes until manually stopped...\n");
+    LogHook.warn(hookConfig.name, `Starting the generator [24/7] !`);
 
-    term.yesOrNo({
-        yes: ["y", "ENTER"],
-        no: ["n"]
-    }, function (error, result) {
 
+    // term.red("No proxy detected...\n");
+    // var progressBar, progress = 0;
+    // function doProgress() {
 
-        if (result) {
+    //     progress += Math.random() / 10;
+    //     progressBar.update(progress);
 
+    //     if (progress >= 1) {
 
-            term.brightYellow(
-                "Want to autostop the generator? [Y|N]\n"
-            );
+    //         console.clear();
+    //         setTimeout(function () {
 
-            term.yesOrNo({
-                yes: ["y", "ENTER"],
-                no: ["n"]
-            }, function (error, result) {
-                if (result) {
-                    toMatch = 0;
+    //             term.green("-------------------------------------\n");
+    //         }, 2000);
+    //         setTimeout(function () {
+    //             term.brightCyan(
+    //                 "||===== Generating Stuff ====|| \n"
+    //             );
+    //         }, 4000);
 
-                    term.brightYellow(
-                        "Want to run the generator on proxies? [Y|N]\n"
-                    );
 
-                    term.yesOrNo({
-                        yes: ["y", "ENTER"],
-                        no: ["n"]
-                    }, function (error, result) {
-                        if (result) {
+    //         term.green("-------------------------------------\n");
+    //         term.green(`Discord nitro generator ${version} \n`);
+    //         term.green(`Checking a code every ${12 / triesPerSecond} second(s)\n`);
 
+    //         setTimeout(function () {
 
+    //             setInterval(() => {
+    //                 checkCodeOffline(generatecode());
+    //             }, (12 / triesPerSecond) * 1000);
+    //         }, 12000);
 
-                            term.cyan("Now using proxies...\n");
+    //     } else {
+    //         setTimeout(doProgress, 100 + Math.random() * 400);
+    //     }
+    // }
 
-                            var progressBar, progress = 0;
 
+    // progressBar = term.progressBar({
+    //     width: 80,
+    //     title: "Starting generator....",
+    //     eta: true,
+    //     percent: true
+    // });
 
-                            function doProgress() {
+    // doProgress();
+// ======== Defferent Area ====== 
 
-                                // Add random progress
-                                progress += Math.random() / 10;
-                                progressBar.update(progress);
+var progressBar, progress = 0;
 
-                                if (progress >= 1) {
 
-                                    console.clear();
-                                    setTimeout(function () {
-                                        term.cyan("Now using proxies...\n");
-                                        term.green("-------------------------------------\n");
-                                    }, 2000);
+function doProgress() {
 
-                                    setTimeout(function () {
-                                        term.brightCyan("Press 'n' to stop the generator at any time\n");
-                                    }, 8000);
+    // Add random progress
+    progress += Math.random() / 10;
+    progressBar.update(progress);
 
+    if (progress >= 1) {
 
-                                    term.green("-------------------------------------\n");
-                                    term.green(`Discord nitro generator ${version} \n`);
-                                    term.green(`Checking a code every ${1 / triesPerSecond} second(s)\n`);
+        console.clear();
+        setTimeout(function () {
+            term.cyan("Now using proxies...\n");
+            term.green("-------------------------------------\n");
+            term.brightCyan("Maintained by Biswa#1234\n");
+        }, 2000);
 
-                                    setTimeout(function () {
+        setTimeout(function () {
+            term.brightCyan("Press 'n' to stop the generator at any time\n");
+        }, 4000);;
 
-                                        setInterval(() => {
-                                            checkCode(generatecode());
-                                        }, (1 / triesPerSecond) * 250);
-                                    }, 12000);
+        term.green("-------------------------------------\n");
+        term.green(`Discord nitro generator ${version} \n`);
+        term.green(`Checking a code every ${1 / triesPerSecond} second(s)\n`);
 
-                                } else {
-                                    setTimeout(doProgress, 100 + Math.random() * 400);
-                                }
-                            }
+        setTimeout(function () {
 
+            setInterval(() => {
+                checkCode(generatecode());
+            }, (1 / triesPerSecond) * 250);
+        }, 12000);
 
-                            progressBar = term.progressBar({
-                                width: 80,
-                                title: "Starting generator....",
-                                eta: true,
-                                percent: true
-                            });
+    } else {
+        setTimeout(doProgress, 100 + Math.random() * 400);
+    }
+}
 
-                            doProgress();
 
+progressBar = term.progressBar({
+    width: 80,
+    title: "Starting generator....",
+    eta: true,
+    percent: true
+});
 
+doProgress();
 
-                        } else {
-                            term.red("'No' detected, not using proxies...\n");
 
-
-                            var progressBar, progress = 0;
-
-
-                            function doProgress() {
-
-                                // Add random progress
-                                progress += Math.random() / 10;
-                                progressBar.update(progress);
-
-                                if (progress >= 1) {
-
-                                    console.clear();
-                                    setTimeout(function () {
-
-                                        term.green("-------------------------------------\n");
-                                        term.brightCyan("Made by Biswa#1234\n");
-                                    }, 2000);
-
-
-                                    term.green("-------------------------------------\n");
-                                    term.green(`Discord nitro generator ${version} \n`);
-                                    term.green(`Checking a code every ${12 / triesPerSecond} second(s)\n`);
-
-                                    setTimeout(function () {
-
-                                        setInterval(() => {
-                                            checkCodeOffline(generatecode());
-                                        }, (12 / triesPerSecond) * 1000);
-                                    }, 12000);
-
-                                } else {
-                                    setTimeout(doProgress, 100 + Math.random() * 400);
-                                }
-                            }
-
-
-                            progressBar = term.progressBar({
-                                width: 80,
-                                title: "Starting generator....",
-                                eta: true,
-                                percent: true
-                            });
-
-                            doProgress();
-
-
-                        }
-                    });
-
-
-                } else {
-                    term.brightYellow("'No' generator will continually log codes until manually stopped...\n");
-                    LogHook.warn(hookConfig.name,`Generator will run 24*7 !`);
-
-                    term.brightYellow(
-                        "Would you like you to enable the use of proxy's? [Y|N]\n"
-                    );
-
-                    term.yesOrNo({
-                        yes: ["y", "ENTER"],
-                        no: ["n"]
-                    }, function (error, result) {
-                        if (result) {
-
-
-
-                            term.cyan("Now using proxies...\n");
-
-                            var progressBar, progress = 0;
-
-
-                            function doProgress() {
-
-                                // Add random progress
-                                progress += Math.random() / 10;
-                                progressBar.update(progress);
-
-                                if (progress >= 1) {
-
-                                    console.clear();
-                                    setTimeout(function () {
-                                        term.cyan("Now using proxies...\n");
-                                        term.green("-------------------------------------\n");
-                                        term.brightCyan("Maintained by Biswa#1234\n");
-                                    }, 2000);
-
-                                    setTimeout(function () {
-                                        term.brightCyan("Press 'n' to stop the generator at any time\n");
-                                    }, 4000);;
-
-                                    term.green("-------------------------------------\n");
-                                    term.green(`Discord nitro generator ${version} \n`);
-                                    term.green(`Checking a code every ${1 / triesPerSecond} second(s)\n`);
-
-                                    setTimeout(function () {
-
-                                        setInterval(() => {
-                                            checkCode(generatecode());
-                                        }, (1 / triesPerSecond) * 250);
-                                    }, 12000);
-
-                                } else {
-                                    setTimeout(doProgress, 100 + Math.random() * 400);
-                                }
-                            }
-
-
-                            progressBar = term.progressBar({
-                                width: 80,
-                                title: "Starting generator....",
-                                eta: true,
-                                percent: true
-                            });
-
-                            doProgress();
-
-
-
-                        } else {
-                            term.red("No proxy detected...\n");
-
-
-                            var progressBar, progress = 0;
-
-
-                            function doProgress() {
-
-                                // Add random progress
-                                progress += Math.random() / 10;
-                                progressBar.update(progress);
-
-                                if (progress >= 1) {
-
-                                    console.clear();
-                                    setTimeout(function () {
-
-                                        term.green("-------------------------------------\n");
-                                    }, 2000);
-                                    setTimeout(function () {
-                                        term.brightCyan(
-                                            "Press 'n' to stop the generator \n"
-                                        );
-                                    }, 4000);
-
-
-                                    term.green("-------------------------------------\n");
-                                    term.green(`Discord nitro generator ${version} \n`);
-                                    term.green(`Checking a code every ${12 / triesPerSecond} second(s)\n`);
-
-                                    setTimeout(function () {
-
-                                        setInterval(() => {
-                                            checkCodeOffline(generatecode());
-                                        }, (12 / triesPerSecond) * 1000);
-                                    }, 12000);
-
-                                } else {
-                                    setTimeout(doProgress, 100 + Math.random() * 400);
-                                }
-                            }
-
-
-                            progressBar = term.progressBar({
-                                width: 80,
-                                title: "Starting generator....",
-                                eta: true,
-                                percent: true
-                            });
-
-                            doProgress();
-
-
-                        }
-                    });
-
-
-                }
-            });
-
-        } else {
-            LogHook.err(hookConfig.name,`generator stopped working !`);
-            term.red("'No' detected, now quitting generator...\n");
-            setTimeout(()=> {process.exit()}, 1200);
-        }
-    });
 }
 
 main()
 process.on('uncaughtException', function (err) {
-    LogHook.err(hookConfig.name,`An error occurred ${err}`);
+    LogHook.err(hookConfig.name, `An error occurred ${err}`);
     term.gray("An error occurred:\n");
     term.gray(err + "\n");
 });
